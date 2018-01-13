990b47cc0b40636584c03db25cce43d1460afcf7
旋風之音 (GoneTone) <p29022716@gmail.com>
2018-01-13 17:04:01 +0800

    Added day

--- functions.php
+++ functions.php
@@ -15,34 +15,39 @@ function time_out($date){
     $hours = $today["hours"]; //時
     $minutes = $today["minutes"]; //分
     $seconds = $today["seconds"]; //秒
-    
+
     $time1 = $year."-".$month."-".$day." ".$hours.":".$minutes.":".$seconds;
     $time2 = $date;
 
     $total_second = (strtotime($time1) - strtotime($time2)); //計算相差之秒數
 
     if($total_second == 0){
-        $time_out = "剛剛";
+        $time_out = "剛剛"; //顯示剛剛
     }
-    
+
     if($total_second > 0 && $total_second < 60){
-        $time_out = $total_second." 秒前";
+        $time_out = $total_second." 秒前"; //顯示秒鐘
     }
 
     if($total_second >= 60 && $total_second < (60*60)) {
         $result = $total_second / 60;
-        $time_out = intval($result)." 分鐘前";
-    }
- 
-    if($total_second >= (60*60) && $total_second < (60*60*24)) {
-        $result = $total_second / (60*60);
-        $time_out = intval($result)." 小時前";
-    }
- 
-    if($total_second >= (60*60*24)) {
-        $time_out = $time2;
+        $time_out = intval($result)." 分鐘前"; //顯示分鐘
     }
 
-    echo $time_out;
+    if($total_second >= (60*60) && $total_second < (60*60*24)) {
+        $result = $total_second / (60*60);
+        $time_out = intval($result)." 小時前"; //顯示小時
+    }
+
+    if($total_second >= (60*60*24) && $total_second < (60*60*24*7)) {
+        $result = $total_second / (60*60*24);
+        $time_out = intval($result)." 天前"; //顯示天日
+    }
+
+    if($total_second >= (60*60*24)) {
+        $time_out = $time2; //顯示日期時間
+    }
+
+    echo $time_out; //顯示
 }
 ?>
